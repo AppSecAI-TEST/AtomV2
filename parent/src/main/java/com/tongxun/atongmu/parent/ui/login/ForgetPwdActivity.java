4cb5a3ddddbdaff003e86734d02162aacfbd19e4
zlxyz123 <123789zxc>
2017-06-27 00:08:17 +0800

    home

--- parent/src/main/java/com/tongxun/atongmu/parent/ui/login/ForgetPwdActivity.java
+++ parent/src/main/java/com/tongxun/atongmu/parent/ui/login/ForgetPwdActivity.java
@@ -1,15 +1,20 @@
 package com.tongxun.atongmu.parent.ui.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tongxun.atongmu.parent.BaseActivity;
 import com.tongxun.atongmu.parent.R;
 
-public class ForgetPwdActivity extends AppCompatActivity {
+public class ForgetPwdActivity extends BaseActivity<IForgetPwdContract.view,ForgetPwdPresenter> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_pwd);
+    }
+
+    @Override
+    protected ForgetPwdPresenter initPresenter() {
+        return new ForgetPwdPresenter();
     }
 }
--- parent/src/main/java/com/tongxun/atongmu/parent/ui/login/IForgetPwdContract.java
+++ parent/src/main/java/com/tongxun/atongmu/parent/ui/login/IForgetPwdContract.java
@@ -1,8 +1,21 @@
 package com.tongxun.atongmu.parent.ui.login;
 
+import com.tongxun.atongmu.parent.BaseView;
+
 /**
  * Created by Anro on 2017/6/26.
  */
 
 public interface IForgetPwdContract {
+    interface view extends BaseView{
+
+    }
+
+    interface presenter{
+
+    }
+
+    interface interactor{
+
+    }
 }
